a8d65016d3f4a9079738638b87ea8eee3e24f37b
TomDotBat <tom@tomdotbat.dev>
2020-12-12 16:57:04 +0000

    Call the presence setter at the right point

--- src/classes/leaderboard.ts
+++ src/classes/leaderboard.ts
@@ -129,14 +129,14 @@ export default class Leaderboard {
         let leaderboardMsg: Message | undefined = await this.findLeaderboard();
         let leaderboard = await this.getLeaderboard();
 
-        if (leaderboardMsg) {leaderboardMsg.edit({embed: leaderboard.getEmbed(this.config)}); return;}
-        this.channel.send({embed: leaderboard.getEmbed(this.config)});
-
         this.bot.user?.setPresence({activity: {
             name: "AoC Leaderboard - Leader: " + leaderboard.getLeaderName().name,
             type: "WATCHING"
         }, status: "online"})
         .catch(console.error);
+
+        if (leaderboardMsg) {leaderboardMsg.edit({embed: leaderboard.getEmbed(this.config)}); return;}
+        this.channel.send({embed: leaderboard.getEmbed(this.config)});
 
         this.lastLeaderboard = leaderboard;
     }
